eb086af878d8cccf9dc96d07161fed0b31a673b0
bottles10 <henryaovare@gmail.com>
2024-06-11 07:51:25 +0000

    Add before callback to check count negativity

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -3,4 +3,10 @@ class Habit < ApplicationRecord
     validates :count, presence: true
 
     scope :ordered, -> { order(updated_at: :desc) }
+
+     before_update :check_count
+
+    def check_count
+        self.count = 0 if count < 0
+    end
 end
